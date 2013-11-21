146fe797b957e9fa088bf0c73e57df408129b5b4
kocolondon <kingsley@kocolondon.com>
2013-11-21 08:40:25 +0000

    Add some steps for cucumber

---
+++ features/steps/users_steps.rb
@@ -0,0 +1,12 @@
+Given(/^that I have added the following users:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I run a find all method$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should now have a result of three total results$/) do
+  pending # express the regexp above with the code you wish you had
+end
